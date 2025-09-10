3ad8682d29fd472821336887d8a100f0b4c79705
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2025-09-10 17:30:55 +0530

    Update msm8953.mk

--- msm8953.mk
+++ msm8953.mk
@@ -50,9 +50,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.print.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.print.xml
 
-# ANT
-PRODUCT_PACKAGES += \
-    AntHalService
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -191,9 +188,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi_msm8953
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -278,9 +272,7 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# TimeKeep
-PRODUCT_PACKAGES += \
-    TimeKeep
+
 
 # USB HAL
 PRODUCT_PACKAGES += \
